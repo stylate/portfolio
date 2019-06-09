aec6f42b4f970659a2f8d390b41cff41d353bb90
Alan Nguyen <nguyenalan@berkeley.edu>
2019-06-09 16:00:41 -0400

    begin modularizing content/photos

---
+++ src/content/dream.svelte
@@ -0,0 +1,10 @@
+<script>
+    let dream = {
+        title: "fleeting dream",
+        files: [
+            './1.jpg',
+            './2.jpg'
+        ]
+    };
+    export dream;
+</script>
---
+++ src/content/fleeting-dream.svelte
@@ -0,0 +1,10 @@
+<script>
+    let dream = {
+        title: "fleeting dream",
+        files: [
+            './photos/dream/1.jpg',
+            './photos/dream/2.jpg'
+        ]
+    };
+    export dream;
+</script>
