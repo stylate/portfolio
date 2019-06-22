8c1e53d8a96e38254a3c70987599f5b766de7960
Alan Nguyen <nguyenalan@berkeley.edu>
2019-06-22 15:53:34 -0400

    fix spotify syntax

--- src/components/Icons.svelte
+++ src/components/Icons.svelte
@@ -20,7 +20,7 @@
 <span>
     <a href="https://instagram.com/alanstheory"><i class="fab fa-instagram"></i></a>&nbsp;
     <a href="https://flexcy.tumblr.com"><i class="fab fa-tumblr"></i></a>&nbsp;
-    <a href="https://open.spotify.com/user/flexcy_?si=eTV7b0eBRj6lJbUfqLmHCQ<Paste>"><i class="fab fa-spotify"></i></a>&nbsp;
+    <a href="https://open.spotify.com/user/flexcy_?si=eTV7b0eBRj6lJbUfqLmHCQ"><i class="fab fa-spotify"></i></a>&nbsp;
     <a href="https://soundcloud.com/stylate"><i class="fab fa-soundcloud"></i></a>&nbsp;
 	<a href="https://letterboxd.com/carbine"><i class="fas fa-film"></i></a>
 </span>
