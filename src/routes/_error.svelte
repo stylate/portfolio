0051599de9015392e03068c0606c3acf6ebb35ee
Alan Nguyen <nguyenalan@berkeley.edu>
2019-06-22 20:00:56 -0400

    change loader

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -5,28 +5,6 @@
 	const dev = process.env.NODE_ENV === 'development';
 </script>
 
-<style>
-    h1, p {
-        margin-top: 40%;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		font-weight: 700;
-	}
-
-	p {
-        margin: 1em auto;
-        display: block;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
 	<title>{status}</title>
 </svelte:head>
