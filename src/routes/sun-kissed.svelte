7a3c43be44a6c2576d00406757a26103b38cf7a6
Alan Nguyen <nguyenalan@berkeley.edu>
2019-06-25 19:38:04 -0400

    refactored folder names, optimized image files

--- src/routes/sun-kissed.svelte
+++ src/routes/sun-kissed.svelte
@@ -1,7 +1,7 @@
 <script>
     import Gallery from '../components/Gallery.svelte';
     let pictures = {
-        id: "sun",
+        id: "sun-kissed",
         number: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
     };
     let name = "sun-kissed";
